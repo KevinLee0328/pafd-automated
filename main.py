174ffa7d8372a3079f3d6f26dbd0cf7e3f3e3b04
Kevin Lee <63915064+KevinLee0328@users.noreply.github.com>
2022-11-25 23:06:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -153,7 +153,7 @@ class Zlapp(Fudan):
             position = last_info["d"]["info"]['geo_api_info']
             position = json_loads(position)
 
-            print("◉上一次提交地址为:", position['formattedAddress'])
+            # print("◉上一次提交地址为:", position['formattedAddress'])
             # print("◉上一次提交GPS为", position["position"])
         # print(last_info)
         
